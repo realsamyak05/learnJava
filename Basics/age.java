470081f25dd2f7452e57cc4f5146ed472a401324
realsamyak05 <samyakchaturvedi11@gmail.com>
2026-08-21 12:04:31 +0530

    initial basics

---
+++ Basics/age.java
@@ -0,0 +1,11 @@
+public class age{
+    public age(){
+
+    }
+    public static void main(String[] args) {
+        int age;
+        age  = 20;
+
+        System.out.println("i am " + age + " years old");
+    }    
+}
